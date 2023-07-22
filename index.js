97d65245b6a8948ad76dc415d49eca3b4a94ca6c
Aure <turduf3@gmail.com>
2023-07-22 12:01:26 +0200

    add css and js

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const change = () => {
+  let docToChange = document.getElementById("benvenuto");
+  let secondDoc = document.getElementById("username");
+  docToChange.style.display = "none";
+  secondDoc.style.display = "flex";
+};
