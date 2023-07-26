84851041913e0feb6cf383fc5c4406670aecd65c
Aure <turduf3@gmail.com>
2023-07-26 06:56:32 +0200

    modified all input and add some function in js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 "use strict";
 
-const change = (docToChangeId,showElementId) => {
+const change = (docToChangeId, showElementId) => {
   let docToChange = document.getElementById(docToChangeId);
   let showElement = document.getElementById(showElementId);
   docToChange.style.display = "none";
   showElement.style.display = "flex";
-  console.log("ok")
+};
+
+const takeValue = () => {
+  let takeUsername = document.getElementById("usernameValue");
+  let takenUsername = document.getElementById("userValue");
+  let takeRepository = document.getElementById("repositoryValue");
+  let takenRepository = document.getElementById("repoValue");
+
+  const valuetoTransfer = takeUsername.value;
+  const repoValueToTransfer = takeRepository.value;
+  if (valuetoTransfer === "") {
+    takenUsername.textContent = "/ Username";
+  } else {
+    takenUsername.textContent = valuetoTransfer;
+  }
+
+  if (repoValueToTransfer === "") {
+    takenRepository.textContent = "/ Repository";
+  } else {
+    takenRepository.textContent = repoValueToTransfer;
+  }
 };
